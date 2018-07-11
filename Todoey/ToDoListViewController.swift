8a23a49699fab791aed7da6f007ba9d393e3d147
Александр Скороткин <killko_judo@mail.ru>
2018-07-11 23:47:24 +0300

    "Add New Item" functionality complete

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Юра Спать", "Саша Заниматься", "Санька Заниматься"]
+    var itemArray = ["Юра Спать", "Саша Заниматься", "Санька Заниматься"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    //MARK - TableView Datasource Methods
+    //MARK - TableView Datasource Methods(Методы)
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -32,7 +32,7 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-    //MARK - TableView Delegate Methods
+    //MARK - TableView Delegate Methods(Методы)
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
@@ -45,6 +45,34 @@ class ToDoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add new Items(Добавление элементов)
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Добавить новый элемент в Todoey", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Добавить", style: .default) { (action) in
+            
+            //What will heppend when user press "Добавить" button(Здесь добавим что произойдет когда будет нажата кнопка после алерта)
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Создать новый элемент"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
     
 }
 
